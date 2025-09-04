0fcded92dbc717c31c60f86106b04aea7808fbcd
FrancineDelanni02 <delannifrancine@gmail.com>
2025-09-04 18:04:01 -0300

    add comentário

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,6 +1,16 @@
 class AbrigoAnimais {
 
+  /* 
+  1) O animal vai para a pessoa que mostrar todos seus brinquedos favoritos na ordem desejada
+  2) Uma pessoa pode intercalar brinquedos que o animal queira ou não, desde que estejam na ordem desejada
+  3) Gatos não dividem seus brinquedos
+  4) Se ambas as pessoas tiverem condições de adoção, ninguém fica com o animal (tadinho)
+  5) Uma pessoa não pode levar mais de três animais para casa
+  6) Loco não se importa com a ordem dos seus brinquedos desde que tenha outro animal como companhia 
+  */
+
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
+    
   }
 }
 
