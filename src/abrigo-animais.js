685c1875544600909113c152d26fd1ed1063cb7c
FrancineDelanni02 <delannifrancine@gmail.com>
2025-09-04 17:42:52 -0300

    ajeitando merge

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class AbrigoAnimais {
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
@@ -6,12 +5,4 @@ class AbrigoAnimais {
 }
 
 export { AbrigoAnimais as AbrigoAnimais };
-=======
-class AbrigoAnimais {
 
-  encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
-  }
-}
-
-export { AbrigoAnimais as AbrigoAnimais };
->>>>>>> 0734e355664615e083e939f95343ae0ee043c753
